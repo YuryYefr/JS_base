1a9347bfe85cf65cb48ee819df05cb80f67a1866
Yuri Yefremov <yuryyefr@gmail.com>
2020-03-26 17:21:09 +0200

    Deep Equal added

--- deep_equal.js
+++ deep_equal.js
@@ -1,89 +1,91 @@
 var first = {
-  name: "Dima",
-  age: 25,
-  isAdmin: false,
-  canSpeak: true,
-  get: function(a, b) {
-    return a + b;
-  },
-  friendNames: ["Саша", "Дмитрий", "Оля"],
-  parents: {
-    mother: {
-      name: "Катя",
-      hobbies: ["Пляж", "Фото", "Море"],
-      friends: [
-        {
-          name: "Петр",
-          age: 49
-        }
-      ]
+    name: "Dima",
+    age: 25,
+    isAdmin: false,
+    canSpeak: true,
+    get: function (a, b) {
+        return a + b;
     },
+    friendNames: ["Саша", "Дмитрий", "Оля"],
+    parents: {
+        mother: {
+            name: "Катя",
+            hobbies: ["Пляж", "Фото", "Море"],
+            friends: [
+                {
+                    name: "Петр",
+                    age: 49
+                }
+            ]
+        },
 
-    father: {
-      name: "Виктор",
-      hobbies: ["Бег", "Солнце", "Туризм"],
-      friends: [
-        {
-          name: "Алексей",
-          age: 40
+        father: {
+            name: "Виктор",
+            hobbies: ["Бег", "Солнце", "Туризм"],
+            friends: [
+                {
+                    name: "Алексей",
+                    age: 40
+                }
+            ]
         }
-      ]
     }
-  }
 };
 var second = {
-  name: "Dima",
-  age: 25,
-  isAdmin: false,
-  canSpeak: true,
-  get: function(a, b) {
-    return a + b;
-  },
-  friendNames: ["Саша", "Дмитрий", "Оля"],
-  parents: {
-    mother: {
-      name: "Катя",
-      hobbies: ["Пляж", "Фото", "Море"],
-      friends: [
-        {
-          name: "Петр",
-          age: 49
-        }
-      ]
+    name: "Dima",
+    age: 25,
+    isAdmin: false,
+    canSpeak: true,
+    get: function (a, b) {
+        return a + b;
     },
+    friendNames: ["Саша", "Дмитрий", "Оля"],
+    parents: {
+        mother: {
+            name: "Катя",
+            hobbies: ["Пляж", "Фото", "Море"],
+            friends: [
+                {
+                    name: "Петр",
+                    age: 49
+                }
+            ]
+        },
 
-    father: {
-      name: "Виктор",
-      hobbies: ["Бег", "Солнце", "Туризм"],
-      friends: [
-        {
-          name: "Алексей",
-          age: 40
+        father: {
+            name: "Виктор",
+            hobbies: ["Бег", "Солнце", "Туризм"],
+            friends: [
+                {
+                    name: "Алексей",
+                    age: 40
+                }
+            ]
         }
-      ]
     }
-  }
 };
 
-function deepEqual(obj, obj2){
-    if (Object.keys(obj).length !== Object.keys(obj2).length){
+function deepEqual(obj, obj2) {
+    if (Object.keys(obj).length !== Object.keys(obj2).length) {
         return false
-    } else if((typeof obj == "object" && obj != null) && (typeof obj2 == "object" && obj2 != null)){
-        for (i in obj){
+    } else if ((typeof obj == "object" && obj != null) && (typeof obj2 == "object" && obj2 != null)) {
+        for (i in obj) {
             if (obj2.hasOwnProperty(i)) {
                 for (i in obj) {
                     if (!deepEqual(obj[i], obj2[i])) {
                         return false;
                     }
-                } return true;
+                }
+                return true;
             } else return false;
         }
         return true;
     } else {
-        if (obj.toString() !== obj2.toString()){
+        if (obj.toString() !== obj2.toString()) {
             return false;
         }
     }
-   return true;
+    return true;
 }
+
 console.log(deepEqual(first, second))
--- hw_4.js
+++ hw_4.js
@@ -2,19 +2,23 @@
 //for обычный и forEach. Реализовать через функцию, которая на вход принимает
 //массив.
 numList = [2, 5, 7, 8, 10]
-function sumGetSimple(someList){
+
+function sumGetSimple(someList) {
     let res = 0;
-    for (i = 0; i < someList.length; i++){
+    for (i = 0; i < someList.length; i++) {
         res += someList[i];
     }
     return res
 }
+
 console.log(sumGetSimple(numList))
 let res = 0;
-function sumGetEach(num){
+
+function sumGetEach(num) {
     res += num;
     return res;
 }
+
 numList.forEach(sumGetEach);
 console.log(res);
 //2. Функция принимает два массива.
@@ -22,52 +26,59 @@ console.log(res);
 //Реализовать через forEach и for
 let first = [2, 3]
 let second = [3, 2]
+
 //result = [5, 5];
-function sumGetTwoSimple(someList, anotherList){
+function sumGetTwoSimple(someList, anotherList) {
     let res = [];
-    if (someList.length !== anotherList.length){
+    if (someList.length !== anotherList.length) {
         alert('arrays must be equal!')
         return
     }
-    for (i = 0; i < someList.length; i++){
+    for (i = 0; i < someList.length; i++) {
         res.push(someList[i] + anotherList[i]);
     }
     return res;
 }
+
 console.log(sumGetTwoSimple(first, second))
 res = []
-function sumGetEachTwo(someList, i){
+
+function sumGetEachTwo(someList, i) {
     res.push(someList + second[i])
     return res;
 }
+
 first.forEach(sumGetEachTwo)
 console.log(res)
 let secondList = [54, 67, 43, 32, 4]
 let thirdList = [32, 56, 67, 89, 10]
 //3.  Есть 3 массива, посчитать их общую сумму. На выходе одно число.
 //  Должна быть функция, которая принимае эти 3 массива, на выходе возвращает 1 число
-function getSumThreeArrays(someList, anotherList, thirdList){
+function getSumThreeArrays(someList, anotherList, thirdList) {
     res = 0;
-    for (i = 0; i < someList.length; i++){
+    for (i = 0; i < someList.length; i++) {
         res += someList[i] + anotherList[i] + thirdList[i];
     }
     return res;
 }
+
 console.log(getSumThreeArrays(numList, secondList, thirdList))
 //4. Есть массив [1, 4, 5, 1, 3, 3] На основе его создать новый массив, где
 //будут квадрат каждого число, используя for и map
 squareArr = [1, 4, 5, 1, 3, 3];
 
-function getSquares(someList){
+function getSquares(someList) {
     res = [];
-    for (i = 0; i < someList.length; i++){
-    res.push(someList[i] * someList[i]);
+    for (i = 0; i < someList.length; i++) {
+        res.push(someList[i] * someList[i]);
     }
     return res
 }
-function getSquaresMap(someNum){
+
+function getSquaresMap(someNum) {
     return someNum * someNum;
 }
+
 console.log(getSquares(squareArr))
 res = squareArr.map(getSquaresMap)
 console.log(res)
@@ -75,20 +86,23 @@ console.log(res)
 //Создать новый массив на выходе, который вернет имена, которые меньше 4 букв
 //Цикл for метод filter
 someNamesList = ['some', 'moresome', 'triplesome', 'try']
-function getThreeCharNames(namesList){
+
+function getThreeCharNames(namesList) {
     resNames = []
-    for (i = 0; i < namesList.length; i++){
+    for (i = 0; i < namesList.length; i++) {
         if (namesList[i].length < 4) {
-        resNames.push(namesList[i]);
-        }else {
-           continue;
+            resNames.push(namesList[i]);
+        } else {
+            continue;
         }
     }
     return resNames;
 }
-function threeCharFilter(someItem){
+
+function threeCharFilter(someItem) {
     return someItem.length < 4;
 }
+
 console.log(getThreeCharNames(someNamesList))
 res = someNamesList.filter(threeCharFilter)
 console.log(res)
